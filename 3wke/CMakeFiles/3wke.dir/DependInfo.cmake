f59e49dfbc42bd4e6fd241b63c6d77e0c1ac4d7d
Steven <stevenw@lanl.gov>
2022-12-07 16:15:15 -0700

    first commit

---
+++ 3wke/CMakeFiles/3wke.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/vast/home/stevenw/WKE/3wke/3wke.cpp" "/vast/home/stevenw/WKE/3wke/CMakeFiles/3wke.dir/3wke.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "HAVE_CUDA=1"
+  "HAVE_KOKKOS=1"
+  "KOKKOS_DEPENDENCE"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "MATAR/src"
+  "3wke"
+  "MATAR/src/install-kokkos-cuda/kokkos/include"
+  "/projects/darwin-nv/rhel8/aarch64/packages/cuda/11.4.0/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ 3wke/CMakeFiles/3wke.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "3wke"
+  "3wke.pdb"
+  "CMakeFiles/3wke.dir/3wke.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/3wke.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
